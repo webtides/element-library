94fa410e980d3d7a4e94f27560f5df21cdb76664
Eddy Löwen <mail@eddyloewen.com>
2020-07-30 13:45:33 +0200

    [slider-element] implements itemsToShow and itemsToScroll options

--- packages/interactions/slider-element/stories/slider-element.stories.js
+++ packages/interactions/slider-element/stories/slider-element.stories.js
@@ -23,3 +23,31 @@ export const rewindVariant = () => html`
 	</slider-element>
 `;
 
+export const itemsToShowVariant = () => html`
+    <slider-element items-to-show="3">
+    	<li class="item bg-red-100 p-4 h-64">1</li>
+    	<li class="item bg-red-200 p-4 h-64">2</li>
+    	<li class="item bg-red-300 p-4 h-64">3</li>
+    	<li class="item bg-red-400 p-4 h-64">4</li>
+    	<li class="item bg-red-500 p-4 h-64">5</li>
+    	<li class="item bg-red-600 p-4 h-64">6</li>
+    	<li class="item bg-red-700 p-4 h-64">7</li>
+    	<li class="item bg-red-800 p-4 h-64">8</li>
+    	<li class="item bg-red-900 p-4 h-64">9</li>
+	</slider-element>
+`;
+
+export const itemsToScrollVariant = () => html`
+    <slider-element items-to-show="3" items-to-scroll="3">
+    	<li class="item bg-red-100 p-4 h-64">1</li>
+    	<li class="item bg-red-200 p-4 h-64">2</li>
+    	<li class="item bg-red-300 p-4 h-64">3</li>
+    	<li class="item bg-red-400 p-4 h-64">4</li>
+    	<li class="item bg-red-500 p-4 h-64">5</li>
+    	<li class="item bg-red-600 p-4 h-64">6</li>
+    	<li class="item bg-red-700 p-4 h-64">7</li>
+    	<li class="item bg-red-800 p-4 h-64">8</li>
+    	<li class="item bg-red-900 p-4 h-64">9</li>
+	</slider-element>
+`;
+
