42bc8ad0db7e00490d96f35e3dc6df6c3a6e9bf8
Patrick Allisat <pallisat@dev5310.com>
2020-08-03 21:30:09 +0200

    WIP adds accordion-group;

---
+++ packages/elements/accordion-group/src/accordion-group.js
@@ -0,0 +1,32 @@
+import { BaseElement, defineElement } from '@webtides/element-js';
+
+export default class AccordionGroup extends BaseElement {
+	constructor() {
+		super();
+		this._children = [];
+	}
+
+	hooks() {
+		return {
+			connected: () => {
+				this._children = Array.from(this.children);
+			},
+		};
+	}
+
+	events() {
+		return {
+			document: {
+				['AccordionElementOpen']: (event) => {
+					this._children.map((details) => {
+						details.open = event.target === details;
+					});
+				},
+			},
+		};
+	}
+}
+
+export function define() {
+	defineElement('accordion-group', AccordionGroup);
+}
