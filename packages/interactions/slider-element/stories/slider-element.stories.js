8577f4b7230d27aa856e0c76dbb3dd08c85dd5f0
Markus Hesper <mh@19f.de>
2020-09-10 12:44:03 +0200

    feat(slider): pretty huge slider overhaul .. .

--- packages/interactions/slider-element/stories/slider-element.stories.js
+++ packages/interactions/slider-element/stories/slider-element.stories.js
@@ -9,45 +9,115 @@ export default {
 
 export const defaultVariant = () => html`
     <slider-element>
-    	<li class="item bg-red-500 p-4 h-64">1</li>
-    	<li class="item bg-blue-500 p-4  h-64">2</li>
-    	<li class="item bg-green-500 p-4 h-64">3</li>
+    	<div class="item bg-red-500 p-4 h-64">1</div>
+    	<div class="item bg-blue-500 p-4  h-64">2</div>
+    	<div class="item bg-green-500 p-4 h-64">3</div>
 	</slider-element>
 `;
 
+
 export const rewindVariant = () => html`
     <slider-element rewind="true">
-    	<li class="item bg-red-500 p-4 h-64">1</li>
-    	<li class="item bg-blue-500 p-4  h-64">2</li>
-    	<li class="item bg-green-500 p-4 h-64">3</li>
+    	<div class="item bg-red-500 p-4 h-64">1</div>
+    	<div class="item bg-blue-500 p-4  h-64">2</div>
+    	<div class="item bg-green-500 p-4 h-64">3</div>
 	</slider-element>
 `;
 
 export const itemsToShowVariant = () => html`
     <slider-element items-to-show="3">
-    	<li class="item bg-red-100 p-4 h-64">1</li>
-    	<li class="item bg-red-200 p-4 h-64">2</li>
-    	<li class="item bg-red-300 p-4 h-64">3</li>
-    	<li class="item bg-red-400 p-4 h-64">4</li>
-    	<li class="item bg-red-500 p-4 h-64">5</li>
-    	<li class="item bg-red-600 p-4 h-64">6</li>
-    	<li class="item bg-red-700 p-4 h-64">7</li>
-    	<li class="item bg-red-800 p-4 h-64">8</li>
-    	<li class="item bg-red-900 p-4 h-64">9</li>
+    	<div class="item bg-red-100 p-4 h-64">1</div>
+    	<div class="item bg-red-200 p-4 h-64">2</div>
+    	<div class="item bg-red-300 p-4 h-64">3</div>
+    	<div class="item bg-red-400 p-4 h-64">4</div>
+    	<div class="item bg-red-500 p-4 h-64">5</div>
+    	<div class="item bg-red-600 p-4 h-64">6</div>
+    	<div class="item bg-red-700 p-4 h-64">7</div>
+    	<div class="item bg-red-800 p-4 h-64">8</div>
+    	<div class="item bg-red-900 p-4 h-64">9</div>
+	</slider-element>
+`;
+
+export const itemsToShowStartVariant = () => html`
+    <slider-element items-to-show="3" snap-align="start">
+    	<div class="item bg-red-100 p-4 h-64">1</div>
+    	<div class="item bg-red-200 p-4 h-64">2</div>
+    	<div class="item bg-red-300 p-4 h-64">3</div>
+    	<div class="item bg-red-400 p-4 h-64">4</div>
+    	<div class="item bg-red-500 p-4 h-64">5</div>
+    	<div class="item bg-red-600 p-4 h-64">6</div>
+    	<div class="item bg-red-700 p-4 h-64">7</div>
+    	<div class="item bg-red-800 p-4 h-64">8</div>
+    	<div class="item bg-red-900 p-4 h-64">9</div>
+	</slider-element>
+`;
+export const itemsToShowEndVariant = () => html`
+    <slider-element items-to-show="3" snap-align="end">
+    	<div class="item bg-red-100 p-4 h-64">1</div>
+    	<div class="item bg-red-200 p-4 h-64">2</div>
+    	<div class="item bg-red-300 p-4 h-64">3</div>
+    	<div class="item bg-red-400 p-4 h-64">4</div>
+    	<div class="item bg-red-500 p-4 h-64">5</div>
+    	<div class="item bg-red-600 p-4 h-64">6</div>
+    	<div class="item bg-red-700 p-4 h-64">7</div>
+    	<div class="item bg-red-800 p-4 h-64">8</div>
+    	<div class="item bg-red-900 p-4 h-64">9</div>
 	</slider-element>
 `;
 
 export const itemsToScrollVariant = () => html`
     <slider-element items-to-show="3" items-to-scroll="3">
-    	<li class="item bg-red-100 p-4 h-64">1</li>
-    	<li class="item bg-red-200 p-4 h-64">2</li>
-    	<li class="item bg-red-300 p-4 h-64">3</li>
-    	<li class="item bg-red-400 p-4 h-64">4</li>
-    	<li class="item bg-red-500 p-4 h-64">5</li>
-    	<li class="item bg-red-600 p-4 h-64">6</li>
-    	<li class="item bg-red-700 p-4 h-64">7</li>
-    	<li class="item bg-red-800 p-4 h-64">8</li>
-    	<li class="item bg-red-900 p-4 h-64">9</li>
+    	<div class="item bg-red-100 p-4 h-64">1</div>
+    	<div class="item bg-red-200 p-4 h-64">2</div>
+    	<div class="item bg-red-300 p-4 h-64">3</div>
+    	<div class="item bg-red-400 p-4 h-64">4</div>
+    	<div class="item bg-red-500 p-4 h-64">5</div>
+    	<div class="item bg-red-600 p-4 h-64">6</div>
+    	<div class="item bg-red-700 p-4 h-64">7</div>
+    	<div class="item bg-red-800 p-4 h-64">8</div>
+    	<div class="item bg-red-900 p-4 h-64">9</div>
 	</slider-element>
 `;
 
+
+export const itemsToScrollStartVariant = () => html`
+    <slider-element items-to-show="3" items-to-scroll="3" snap-align="start">
+    	<div class="item bg-red-100 p-4 h-64">1</div>
+    	<div class="item bg-red-200 p-4 h-64">2</div>
+    	<div class="item bg-red-300 p-4 h-64">3</div>
+    	<div class="item bg-red-400 p-4 h-64">4</div>
+    	<div class="item bg-red-500 p-4 h-64">5</div>
+    	<div class="item bg-red-600 p-4 h-64">6</div>
+    	<div class="item bg-red-700 p-4 h-64">7</div>
+    	<div class="item bg-red-800 p-4 h-64">8</div>
+    	<div class="item bg-red-900 p-4 h-64">9</div>
+	</slider-element>
+`;
+
+export const itemsToScrollEndVariant = () => html`
+    <slider-element items-to-show="3" items-to-scroll="3" snap-align="end">
+    	<div class="item bg-red-100 p-4 h-64">1</div>
+    	<div class="item bg-red-200 p-4 h-64">2</div>
+    	<div class="item bg-red-300 p-4 h-64">3</div>
+    	<div class="item bg-red-400 p-4 h-64">4</div>
+    	<div class="item bg-red-500 p-4 h-64">5</div>
+    	<div class="item bg-red-600 p-4 h-64">6</div>
+    	<div class="item bg-red-700 p-4 h-64">7</div>
+    	<div class="item bg-red-800 p-4 h-64">8</div>
+    	<div class="item bg-red-900 p-4 h-64">9</div>
+	</slider-element>
+`;
+
+export const variableWidthVariant = () => html`
+    <slider-element >
+    	<div class="item bg-red-100 p-4 h-64 w-1/2">1</div>
+    	<div class="item bg-red-200 p-4 h-64 w-6/12">2</div>
+    	<div class="item bg-red-300 p-4 h-64 w-8/12">3</div>
+    	<div class="item bg-red-400 p-4 h-64 w-4/12">4</div>
+    	<div class="item bg-red-500 p-4 h-64 w-2/12">5</div>
+    	<div class="item bg-red-600 p-4 h-64w-12/12">6</div>
+    	<div class="item bg-red-700 p-4 h-64">7</div>
+    	<div class="item bg-red-800 p-4 h-64 w-1/12">8</div>
+    	<div class="item bg-red-900 p-4 h-64">9</div>
+	</slider-element>
+`;
