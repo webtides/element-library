cf51e525ddfd7d4c9491439c4ec79d7916264f6b
Markus Hesper <mh@19f.de>
2020-09-18 09:01:26 +0200

    Update packages/interactions/carousel-element/tests/carousel-element.unit.test.js
    Co-authored-by: Eddy Löwen <mail@eddyloewen.com>

--- packages/interactions/carousel-element/tests/carousel-element.unit.test.js
+++ packages/interactions/carousel-element/tests/carousel-element.unit.test.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-unused-expressions */
 import { fixture, defineCE, assert, oneEvent, nextFrame } from '@open-wc/testing';
-import CarouselElement from '../src/slider-element';
+import CarouselElement from '../src/carousel-element';
 
 describe('Unit | SliderElement', () => {
     it('can be created without errors', async () => {
         const el = new CarouselElement;
     });
 });
-
