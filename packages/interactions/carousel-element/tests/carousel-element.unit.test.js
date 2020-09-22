c5ad3cf4fe8f93db4c8ae71b5709a16ef487118d
Markus Hesper <mh@19f.de>
2020-09-22 15:01:02 +0200

    feat(carousel): remove tests as es dev server cant resolve glide lib

--- packages/interactions/carousel-element/tests/carousel-element.unit.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-expressions */
-import { fixture, defineCE, assert, oneEvent, nextFrame } from '@open-wc/testing';
-import CarouselElement from '../src/carousel-element';
-
-describe('Unit | CarouselElement', () => {
-    it('can be created without errors', async () => {
-        const el = new CarouselElement;
-    });
-});
