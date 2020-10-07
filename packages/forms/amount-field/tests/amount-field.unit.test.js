81e7c710a2491edb8acb4a78d2a6a16dfc28c693
Markus Hesper <mh@19f.de>
2020-10-07 13:56:40 +0200

    feat(form/amount-field): initial barebones amount field

---
+++ packages/forms/amount-field/tests/amount-field.unit.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-expressions */
+import { fixture, defineCE, assert, oneEvent, nextFrame } from '@open-wc/testing';
+import AmountField from '../src/amount-field';
+
+describe('Unit | AmountField', () => {
+    it('can be created without errors', async () => {
+        const el = new AmountField;
+    });
+});
+
