2697b641d394adc1282536bda286bd43ca09f1ee
Eddy Löwen <mail@eddyloewen.com>
2020-10-14 13:57:43 +0200

    uses postcss for storybook build mode

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,19 @@
-const postcss = require('postcss');
+const postcss = require('rollup-plugin-postcss');
 
 module.exports = {
-    stories: [
-        '../packages/**/**/stories/*.stories.js',
-    ],
-    addons: [
-        'storybook-prebuilt/addon-actions/register.js',
-        'storybook-prebuilt/addon-knobs/register.js',
-        'storybook-prebuilt/addon-a11y/register.js',
-        'storybook-prebuilt/addon-docs/register.js',
-    ],
-    esDevServer: {
-        compatibility: 'none',
-        nodeResolve: true,
-        watch: true,
-        open: false,
-        babel: true,
+	stories: ['../packages/**/**/stories/*.stories.js'],
+	addons: [
+		'storybook-prebuilt/addon-actions/register.js',
+		'storybook-prebuilt/addon-knobs/register.js',
+		'storybook-prebuilt/addon-a11y/register.js',
+		'storybook-prebuilt/addon-docs/register.js',
+	],
+	esDevServer: {
+		compatibility: 'none',
+		nodeResolve: true,
+		watch: true,
+		open: false,
+		babel: true,
 		plugins: [
 			// create an inline plugin
 			{
@@ -31,13 +29,35 @@ module.exports = {
 						const plugins = require('./postcss.config')().plugins;
 						await postcss(plugins)
 							.process(css, {})
-							.then(result => {
+							.then((result) => {
 								css = result.css;
-							})
+							});
 						return { body: `export default \`${css}\`;` };
 					}
 				},
 			},
 		],
-    },
+	},
+
+	// Rollup build output directory (build-storybook only)
+	// outputDir: '../docs',
+	// Configuration for rollup (build-storybook only)
+	rollup: (config) => {
+		config.plugins = [
+			postcss({
+				plugins: [
+					require('postcss-import')(),
+					require('postcss-nested')(),
+					require('autoprefixer')({
+						grid: true,
+					}),
+				],
+			}),
+			...config.plugins,
+		];
+
+		console.log({ config: config.plugins });
+
+		return config;
+	},
 };
