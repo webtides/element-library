4e6bee9efb365fcc8e0a8512e0314ea9e925b8c5
Eddy Löwen <mail@eddyloewen.com>
2020-11-12 15:00:21 +0100

    [dropdown-element] adds dropdown element
    # Conflicts:
    #	README.md

---
+++ packages/interactions/dropdown-element/tests/dropdown-element.unit.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-expressions */
+import { fixture, defineCE, assert, oneEvent, nextFrame } from '@open-wc/testing';
+import DropdownElement from '../src/dropdown-element';
+
+describe('Unit | DropdownElement', () => {
+    it('can be created without errors', async () => {
+        const el = new DropdownElement;
+    });
+});
+
