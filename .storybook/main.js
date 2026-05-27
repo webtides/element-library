5095a31b43b14c9a522cf3149b6eabb47e936c17
Eddy Löwen <mail@eddyloewen.com>
2026-05-27 11:38:14 +0200

    chore: drop @storybook/addon-coverage
    Was commented out in main.js for a while. Removing the dep also
    clears the vite-plugin-istanbul peer warning against vite 8.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,7 @@
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 const config = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-docs',
-        '@chromatic-com/storybook',
-        '@storybook/addon-a11y',
-        // '@storybook/addon-coverage',
-    ],
+    addons: ['@storybook/addon-links', '@storybook/addon-docs', '@chromatic-com/storybook', '@storybook/addon-a11y'],
     framework: {
         name: '@storybook/web-components-vite',
         options: {},
